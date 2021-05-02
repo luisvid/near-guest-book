dd622bc431f6e44e1111e7ba641e1e110696d529
Luis Videla <luisvid@gmail.com>
2021-05-02 02:55:57 -0300

    set contract name

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME || 'guest-book.testnet';
+// const CONTRACT_NAME = process.env.CONTRACT_NAME || 'guest-book.testnet';
+const CONTRACT_NAME = process.env.CONTRACT_NAME || 'luisvidela.testnet';
 
 function getConfig(env) {
   switch(env) {
